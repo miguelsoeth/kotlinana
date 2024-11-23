0b074a8687a71464e105e29143a2e0761df2d11d
Miguel Soeth <61883339+miguelsoeth@users.noreply.github.com>
2024-11-23 10:42:09 -0300

    Initial commit

---
+++ app/src/main/java/com/example/kotlinana/CaptureActivity.kt
@@ -0,0 +1,10 @@
+package com.example.kotlinana
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class CaptureActivity : com.journeyapps.barcodescanner.CaptureActivity() {
+}
---
+++ app/src/main/java/com/example/kotlinana/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.kotlinana
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.kotlinana.databinding.ActivityMainBinding
+import com.journeyapps.barcodescanner.ScanContract
+import com.journeyapps.barcodescanner.ScanOptions
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.main)
+
+        binding.btnScan.setOnClickListener {
+            scanCode()
+        }
+    }
+
+    private fun scanCode() {
+        val options = ScanOptions().apply {
+            setPrompt("Leia o código")
+            setBeepEnabled(false)
+            setOrientationLocked(true)
+            setCaptureActivity(CaptureActivity::class.java)
+        }
+        barLauncher.launch(options)
+    }
+
+    private val barLauncher = registerForActivityResult(ScanContract()) { result ->
+        if (result.contents != null) {
+            binding.scanResult.text = result.contents.toString()
+        }
+    }
+
+}
